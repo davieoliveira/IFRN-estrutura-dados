4955e57416590c9f91cf40c1690526c732f43813
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2024-03-28 20:23:50 -0300

    começando questão desafio

---
+++ Array_One_Bit_Code/questao desafio/index.js
@@ -0,0 +1,16 @@
+do{
+    const opcao = +prompt('Escolha umas das opções abaixo: \n 1- Chamar Paciente 2- Cadastrar Paciente 3- Sair')
+    
+    if(opcao = 1){
+        alert('Proximo!')
+    }
+    
+    if(opcao = 2){
+        alert('Va para o final da fila')
+    }
+    
+    if(opcao = 3){
+        alert('xau')
+    }
+
+} while(opcao != 3)
