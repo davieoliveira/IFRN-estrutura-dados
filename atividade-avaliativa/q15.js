ff22972b2bcc4593b9815ead23e7d212fab12555
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2024-07-23 17:22:51 -0300

    Começando questão 15

--- atividade-avaliativa/q15.js
+++ atividade-avaliativa/q15.js
@@ -2,3 +2,18 @@
  * 15. Escreva uma função que receba um array e um número alvo, e retorne todos os
  * pares de elementos do array cuja soma seja igual ao número alvo. 
  */
+
+function somados(array, numeroAlvo) {
+    let somas = [] 
+    for(let x = 0 ; array.length > x ; x++) {
+        for(let y = 0 ; array.length > y ; y++){
+            if(array[x] + array[y] == numeroAlvo){
+                somas.push('A soma do número ' + array[x] + ' + ' + array[y] + ' = ' + numeroAlvo)
+            }
+        }
+    }  
+    console.log(somas)
+}
+
+array = [6, 4, 5, 5];
+somados(array, 10)
--- atividade-avaliativa/teste.js
+++ atividade-avaliativa/teste.js
@@ -1,3 +1,22 @@
-let array = [20, 21, 10, 12, 1, 99, 3, 65];
+let array = [10, 2, 1, 1, 3, 5, 10, 1, 1];
+
+// Deixando os arrays em ordem crescente
 array.sort((a, b) => a - b);
-console.log(array);
+let maiorRepeticao = 0;
+let contador = 0;
+let numero = 0;
+
+for(let x = 0 ; array.length > x ; x++) {
+    if(array[x] == array[x+1]){
+        contador += 1;
+    }
+    
+    else{
+        if(contador > maiorRepeticao) {
+            maiorRepeticao = contador;
+            numero = array[x];
+        } 
+    }
+}
+
+console.log(numero);
