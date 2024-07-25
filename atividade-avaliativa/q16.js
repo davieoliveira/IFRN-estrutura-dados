09dd7dae85afc29bbf8928073d12f69b2e84d075
Davi Oliveira <145984104+davieoliveira@users.noreply.github.com>
2024-07-25 17:20:16 -0300

    Terminando questão 16

--- atividade-avaliativa/q16.js
+++ atividade-avaliativa/q16.js
@@ -1,25 +1,20 @@
 // 16. Escreva uma função que encontre o elemento mais frequente em um array.
-function mostRepeated(array){
-    let repeat = 0;
-    let position = 0;
+function mostRepeatedElement(array) {
     let count = 0;
-
-    for(let x = 0 ; array.length > x ; x++) {
+    let highestCount = 0;
+    let index = 0;
+    
+    for(let x = 0 ; array.length > x ; x++){
         for(let y = 0 ; array.length > y ; y++) {
-            
-            let count = 0;
-            
-            if(array[x] == array[y]) {
+            if(array[x] == array[y] && x != y) {
                 count += 1;
+                if(count > highestCount) {
+                    highestCount = count;
+                    index = x;
+                }
             }
         }
-        if(count > repeat) {
-            repeat = count;
-            position = array[x];
-        }
-        count = 0
     }
-    console.log(array[position]);
+
+    return array[index];
 }
-let array = [10, 1, 2, 4, 5, 1, 5, 5, 5, 5, 5];
-mostRepeated(array);
--- atividade-avaliativa/teste.js
+++ atividade-avaliativa/teste.js
@@ -1,22 +1,7 @@
-let array = [10, 2, 1, 1, 3, 5, 10, 1, 1];
+let array = [1, 2, 3, 4];
+for(let x = 0 ; array.length > x ; x++){
+    for(let y = 0 ; array.length > y ; y++) {
+        console.log(array[x], array[y]);
 
-// Deixando os arrays em ordem crescente
-array.sort((a, b) => a - b);
-let maiorRepeticao = 0;
-let contador = 0;
-let numero = 0;
-
-for(let x = 0 ; array.length > x ; x++) {
-    if(array[x] == array[x+1]){
-        contador += 1;
-    }
-    
-    else{
-        if(contador > maiorRepeticao) {
-            maiorRepeticao = contador;
-            numero = array[x];
-        } 
     }
 }
-
-console.log(numero);
